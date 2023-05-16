f2f04b002816ec6a004dd1bd10dbebe6a2d3d9a8
Phil Jirsa <phil.jirsa@microsoft.com>
2023-05-15 20:37:46 -0500

    Add storage account client

--- Api/Program.cs
+++ Api/Program.cs
@@ -2,6 +2,9 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Azure.Functions.Worker.Configuration;
+using Microsoft.Extensions.Azure;
+using System;
+using Azure.Identity;
 
 namespace ApiIsolated
 {
@@ -11,6 +14,16 @@ namespace ApiIsolated
         {
             var host = new HostBuilder()
                 .ConfigureFunctionsWorkerDefaults()
+                //.ConfigureFunctionsWorkerDefaults(builder =>
+                //{                    
+                //    //builder.Services.AddAzureClients(clientBuilder =>
+                //    //{
+                //    //    clientBuilder.AddBlobServiceClient(Environment.GetEnvironmentVariable("galleryStorageUrl"));
+
+                //    //    clientBuilder.UseCredential(new DefaultAzureCredential(new DefaultAzureCredentialOptions { TenantId = Environment.GetEnvironmentVariable("AzureTenantId")}));
+                //    //});
+                //})
+                
                 .Build();
 
             host.Run();
--- Api/ScorecardFunction.cs
+++ Api/ScorecardFunction.cs
@@ -5,31 +5,40 @@ using Microsoft.Extensions.Logging;
 using BlazorApp.Shared;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
+using System;
 
 namespace Api
 {
     public class ScorecardFunction
     {
         private readonly ILogger _logger;
+        private readonly BlobServiceClient _blobService;
 
         public ScorecardFunction(ILoggerFactory loggerFactory)
         {
             _logger = loggerFactory.CreateLogger<ScorecardFunction>();
+            _blobService = new BlobServiceClient(Environment.GetEnvironmentVariable("GalleryStorageConnection"));
         }
 
         [Function("GetScorecards")]
         public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "getScorecards/{weekId}")] HttpRequestData req, 
             int weekId)
         {
+            var client = _blobService.GetBlobContainerClient("scorecards");
             var results = new List<Scorecard>();
+            var sasBuilder = new BlobSasBuilder(BlobContainerSasPermissions.Read, System.DateTimeOffset.UtcNow.AddHours(8));
 
-            for (int i = 0; i < 5; i++)
+            await foreach (BlobItem item in client.GetBlobsAsync())
             {
+                var blob = client.GetBlobClient(item.Name);
                 results.Add(new Scorecard
                 {
                     WeekId = weekId,
-                    Filename = $"Week{weekId}Scorecard{i + 1}.jpg",
-                    ShareLinkWebUrl = $"https://www.google.com/search?q=Week{weekId}Scorecard{i + 1}.jpg"
+                    Filename = item.Name,
+                    ShareLinkWebUrl = blob.GenerateSasUri(sasBuilder).AbsoluteUri
                 });
             }
 
